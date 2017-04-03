741e43d70134a4b50aa37d72bd66bed6af7e7ce4
Sam Kirsch and grinil khanna <sam+grinil@>
2017-04-03 10:49:02 -0700

    Add service, injections, and providers to display specific albums on detials page via id.

--- src/app/album-details/album-details.component.ts
+++ src/app/album-details/album-details.component.ts
@@ -2,22 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { Album } from '../album.model';
+import { AlbumService } from '../album.service';
 
 @Component({
   selector: 'app-album-details',
   templateUrl: './album-details.component.html',
-  styleUrls: ['./album-details.component.css']
+  styleUrls: ['./album-details.component.css'],
+  providers: [AlbumService]
 })
 export class AlbumDetailsComponent implements OnInit {
 
   albumId: number = null;
+  albumToDisplay: Album;
 
-  constructor(private route: ActivatedRoute, private location: Location) { }
+  constructor(private route: ActivatedRoute,
+            private location: Location,
+            private albumService: AlbumService) { }
 
   ngOnInit() {
     this.route.params.forEach((urlParameters) => {
       this.albumId = parseInt(urlParameters['id']);
     });
+    this.albumToDisplay = this.albumService.getAlbumById(this.albumId);
   }
 
 }
