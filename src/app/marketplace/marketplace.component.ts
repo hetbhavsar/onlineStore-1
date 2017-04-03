d915c30a5f98226783700026adeec854b028d44f
Sam Kirsch and grinil khanna <sam+grinil@>
2017-04-03 10:37:30 -0700

    add a album model service injection in marketplace

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Album } from '../album.model';
 import { Router } from '@angular/router';
+import { AlbumService } from '../album.service';
 
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
-  styleUrls: ['./marketplace.component.css']
+  styleUrls: ['./marketplace.component.css'],
+  providers: [AlbumService]
 })
-export class MarketplaceComponent {
-  public albumsArray: Album[] = Album.albums;
+export class MarketplaceComponent implements OnInit {
+  public albumsArray: Album[];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private albumService: AlbumService) {}
+
+  ngOnInit() {
+    this.albumsArray = this.albumService.getAlbums();
+  }
 
 
   goToAlbumDetails(selectedAlbum: Album) {
